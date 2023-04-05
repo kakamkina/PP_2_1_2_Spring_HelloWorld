7ea1c0d8cab24e00c543b22f13fd492c033fe11f
kakamkina <karinakamkina@gmail.com>
2023-04-05 23:54:15 +0300

    +

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,25 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld helloWorldBean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(helloWorldBean.getMessage());
+
+        HelloWorld helloWorldBeanCopy =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(helloWorldBeanCopy.getMessage());
+
+        Cat catBean =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getVoice());
+
+        Cat catBeanCopy =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBeanCopy.getVoice());
+
+        System.out.println(helloWorldBean.equals(helloWorldBeanCopy));
+        System.out.println(catBean.equals(catBeanCopy));
+
+
     }
 }
